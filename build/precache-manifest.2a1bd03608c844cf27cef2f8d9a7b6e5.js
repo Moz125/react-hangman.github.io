2ab3fedeaa3038c1d72ea6109daf00dd3fd5e116
Moz125 <FA16-BCS-021@ISBSTUDENT.COMSATS.EDU.PK>
2020-08-08 14:12:22 +0500

    game added

---
+++ build/precache-manifest.2a1bd03608c844cf27cef2f8d9a7b6e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ae3e6f3522bf470e5fe2d2119f657e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "621b9242e9b9ba97052b",
+    "url": "/static/css/main.5be84a40.chunk.css"
+  },
+  {
+    "revision": "973e31a4495480d1933b",
+    "url": "/static/js/2.5da62bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5da62bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621b9242e9b9ba97052b",
+    "url": "/static/js/main.4bc17a47.chunk.js"
+  },
+  {
+    "revision": "f1659f0006a76036fbb1",
+    "url": "/static/js/runtime-main.f5fd235d.js"
+  }
+]);
